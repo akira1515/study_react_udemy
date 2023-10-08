3a80cc028ad7bf82ac8cd29db92af1b80d1bf648
akira1515 <akira.kobayashi1515@gmail.com>
2023-10-08 14:41:26 +0900

    セクション4 done

--- react-guide-material/04_react_basic/src/100_practice_props/start/Example.js
+++ react-guide-material/04_react_basic/src/100_practice_props/start/Example.js
@@ -3,6 +3,8 @@ import Profile from "./components/Profile";
 const profile = [
   { name: "Takashi", age: 19, country: "Japan" },
   { name: "Jane", age: 28, country: "UK" },
+  // { name: "John Doe", age: "??", country: "Japan" },
+
 ];
 
 const Example = () => {
@@ -11,11 +13,7 @@ const Example = () => {
       <h3>練習問題</h3>
       <p>Profileコンポーネントの中身を変更して完成コードと同じ状態になるようにしてください。※なるべく分割代入を使用してください。
       </p>
-      <Profile
-        name={profile[0].name}
-        age={profile[0].age}
-        country={profile[0].country}
-      />
+      <Profile {...profile[0]} />
       <Profile {...profile[1]} />
       <Profile />
     </div>
