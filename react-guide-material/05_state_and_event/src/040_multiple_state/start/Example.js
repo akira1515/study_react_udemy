0a2b535ecb259b75d4de4f3650ef573cc464b3c4
akira1515 <akira.kobayashi1515@gmail.com>
2023-10-08 20:44:15 +0900

    state

--- react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.js
+++ react-guide-material/05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
 const Example = () => {
-  
+    const [countA, setCountA] = useState(0);
+    const [countB, setCountB] = useState(0);
+    const [countC, setCountC] = useState(0);
+
+    const clickHandlerA = () => {
+        setCountA(countA + 1);
+    }
+    const clickHandlerB = () => {
+        setCountB(countB + 1);
+    }
+    const clickHandlerC = () => {
+        setCountC(countC + 1);
+    }
+    return (
+        <>
+            <h3>ボタンAを{countA}回押しました</h3>
+                <button onClick={clickHandlerA}>ボタンA</button>
+            <h3>ボタンBを{countB}回押しました</h3>
+                <button onClick={clickHandlerB}>ボタンA</button>
+            <h3>ボタンCを{countC}回押しました</h3>
+                <button onClick={clickHandlerC}>ボタンA</button>
+        </>
+    )
 };
 
 export default Example;
--- react-guide-material/05_state_and_event/src/050_prev_state/start/Example.js
+++ react-guide-material/05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,20 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [count, setCount] = useState(0);
+    const countIncrease = () => {
+        setCount(count + 1);
+    }
+    const countDecrease = () => {
+        setCount(count - 1);
+    }
+    return (
+        <>
+            <p>現在のカウント数：{count}</p>
+            <button onClick = {countIncrease}>+</button>
+            <button onClick = {countDecrease}>-</button>
+        </>
+    )
 };
 
 export default Example;
