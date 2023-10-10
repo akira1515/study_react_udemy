1ca2e8eae1dd887dca965f5a431464cc12cbaae4
akira1515 <akira.kobayashi1515@gmail.com>
2023-10-10 15:13:54 +0900

    REST API通信

--- react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
+++ react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Example = () => {
-  return <></>;
+  const [user, setUser] = useState([]);
+
+    useEffect(() => {
+      const getUser = async () => {
+        const res = await axios.get('http://localhost:3003/user')
+        console.log(res)
+      }
+      getUser();
+    })
 };
 
 export default Example;
